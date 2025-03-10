ed52269f992b5f4c6970436fa2e5d19887d523f5
Prince Thakarar <113857771+princethakarar@users.noreply.github.com>
2025-03-10 09:55:20 +0530

    remove API key

--- src/Config/gemini.jsx
+++ src/Config/gemini.jsx
@@ -1,6 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const apiKey = "AIzaSyBFfOMVFqC3zh70lH9-tBlQ5FHM5cGpMVI"; // Replace with your actual API key
+const apiKey = ""; // Replace with your actual API key
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
